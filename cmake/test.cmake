b3f7d7c0f7962aaf7ebd2d34619e177c11ebb3af
Radovan Bast <bast@users.noreply.github.com>
2017-12-13 20:47:19 +0100

    update gtest integration

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -6,10 +6,20 @@ if(ENABLE_UNIT_TESTS)
 
     ExternalProject_Add(
         gtest
-        PREFIX "${PROJECT_BINARY_DIR}/gtest"
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG master
-        INSTALL_COMMAND true  # currently no install command
+        PREFIX
+          "${PROJECT_BINARY_DIR}/gtest"
+        GIT_REPOSITORY
+          https://github.com/google/googletest.git
+        GIT_TAG
+          release-1.8.0
+        CMAKE_ARGS
+          "-Dgtest_disable_pthreads=ON"
+        INSTALL_COMMAND
+          "" # prevent gtest from installing itself system-wide
+        BUILD_BYPRODUCTS
+          # this is here otherwise Ninja will complain
+          # that there is no rule to build libgtest.a
+          ${PROJECT_BINARY_DIR}/gtest/src/gtest-build/googlemock/gtest/libgtest.a
         )
 
     add_executable(
@@ -18,24 +28,23 @@ if(ENABLE_UNIT_TESTS)
         test/calculator.cpp
         )
 
-    target_link_libraries(
-        unit_tests
-        ${PROJECT_BINARY_DIR}/gtest/src/gtest-build/googlemock/gtest/libgtest.a
-        calculator
-        pthread
-        )
-
     target_include_directories(
         unit_tests
         PRIVATE
-        ${PROJECT_BINARY_DIR}/gtest/src/gtest/googletest/include
         ${PROJECT_SOURCE_DIR}/src
+        ${PROJECT_BINARY_DIR}/gtest/src/gtest/googletest/include
         )
 
+    target_link_libraries(
+        unit_tests
+        PRIVATE
+        calculator
+        ${PROJECT_BINARY_DIR}/gtest/src/gtest-build/googlemock/gtest/libgtest.a
+        )
+
+    # make sure that gtest is built before we build unit_tests
     add_dependencies(unit_tests gtest)
 
-    include(CTest)
     enable_testing()
-
     add_test(unit ${PROJECT_BINARY_DIR}/bin/unit_tests)
 endif()
