42692d277ad68aa18c3b3efe4ce186452310f93c
Radovan Bast <bast@users.noreply.github.com>
2018-03-22 10:16:33 +0100

    call the include with a macro

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,22 +1,24 @@
-# in case Git is not available, we default to "unknown"
-set(GIT_HASH "unknown")
+macro(generate_version_h)
+    # in case Git is not available, we default to "unknown"
+    set(GIT_HASH "unknown")
 
-# find Git and if available set GIT_HASH variable
-find_package(Git QUIET)
-if(GIT_FOUND)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} --no-pager show -s --pretty=format:%h -n 1
-        OUTPUT_VARIABLE GIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        WORKING_DIRECTORY
-            ${PROJECT_SOURCE_DIR}
-        ERROR_QUIET
+    # find Git and if available set GIT_HASH variable
+    find_package(Git QUIET)
+    if(GIT_FOUND)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} --no-pager show -s --pretty=format:%h -n 1
+            OUTPUT_VARIABLE GIT_HASH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            WORKING_DIRECTORY
+                ${PROJECT_SOURCE_DIR}
+            ERROR_QUIET
+            )
+    endif()
+
+    # generate file version.h based on version.h.in
+    configure_file(
+        cmake/version.h.in
+        generated/version.h
+        @ONLY
         )
-endif()
-
-# generate file version.h based on version.h.in
-configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/version.h.in
-    generated/version.h
-    @ONLY
-    )
+endmacro()
