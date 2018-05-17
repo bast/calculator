5044e9ec6332623e53a34673f0ccdd4398b193ff
Radovan Bast <bast@users.noreply.github.com>
2018-05-17 10:18:19 +0200

    indentation

--- cmake/arch.cmake
+++ cmake/arch.cmake
@@ -1,11 +1,11 @@
 function(macos_workaround)
-    message(STATUS "My system is ${CMAKE_SYSTEM_NAME}")
-    message(STATUS "My processor is ${CMAKE_HOST_SYSTEM_PROCESSOR}")
+  message(STATUS "My system is ${CMAKE_SYSTEM_NAME}")
+  message(STATUS "My processor is ${CMAKE_HOST_SYSTEM_PROCESSOR}")
 
-    # workaround for CMP0042 warning on Mac
-    if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-        if(NOT DEFINED CMAKE_MACOSX_RPATH)
-            set(CMAKE_MACOSX_RPATH ON)
-        endif()
+  # workaround for CMP0042 warning on Mac
+  if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    if(NOT DEFINED CMAKE_MACOSX_RPATH)
+      set(CMAKE_MACOSX_RPATH ON)
     endif()
+  endif()
 endfunction()
