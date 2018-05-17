f5a44beb584da8420b6353021c0ae8c607a510ee
Radovan Bast <bast@users.noreply.github.com>
2018-05-17 09:32:38 +0200

    wrap macos workaround into a function

--- cmake/arch.cmake
+++ cmake/arch.cmake
@@ -1,8 +1,11 @@
-message(STATUS "My system is ${CMAKE_SYSTEM_NAME} and my processor is ${CMAKE_HOST_SYSTEM_PROCESSOR}")
+function(macos_workaround)
+    message(STATUS "My system is ${CMAKE_SYSTEM_NAME}")
+    message(STATUS "My processor is ${CMAKE_HOST_SYSTEM_PROCESSOR}")
 
-# workaround for CMP0042 warning on Mac
-if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-    if(NOT DEFINED CMAKE_MACOSX_RPATH)
-        set(CMAKE_MACOSX_RPATH ON)
+    # workaround for CMP0042 warning on Mac
+    if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+        if(NOT DEFINED CMAKE_MACOSX_RPATH)
+            set(CMAKE_MACOSX_RPATH ON)
+        endif()
     endif()
-endif()
+endfunction()
